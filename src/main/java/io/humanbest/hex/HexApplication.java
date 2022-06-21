0f9fc3a105504d11685045d2e6a52dfb604ca2a8
95suskjg <95suskjg@naver.com>
2022-06-21 15:09:22 +0900

    kjg

--- src/main/java/io/humanbest/hex/HexApplication.java
+++ src/main/java/io/humanbest/hex/HexApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HexApplication {
+	asdasd
 
 	public static void main(String[] args) {
 		SpringApplication.run(HexApplication.class, args);
