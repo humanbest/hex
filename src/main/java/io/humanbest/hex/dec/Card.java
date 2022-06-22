dc1102f77253d40fc13288879a0b8cf86dfe950f
95suskjg <95suskjg@naver.com>
2022-06-22 15:52:56 +0900

    20220622

--- src/main/java/io/humanbest/hex/dec/Card.java
+++ src/main/java/io/humanbest/hex/dec/Card.java
@@ -2,6 +2,10 @@ package io.humanbest.hex.dec;
 
 public class Card {
 
+    //AT버프는 몬스터에게 피해를 입히는 공격적인 효과를 지닌 버프입니다
+    //DF버프는 수비적인 효과를 얻을 수 있는 버프입니다
+
+
     public static enum Type {
         ATTACK, DEFNSE, ATBUFF, DFBUFF, DEBUFF
     }
