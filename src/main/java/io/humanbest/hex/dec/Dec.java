745fef9413afb876d688d6ef20cdf50d255de08c
95suskjg <95suskjg@naver.com>
2022-06-22 17:28:17 +0900

    20220622

--- src/main/java/io/humanbest/hex/dec/Dec.java
+++ src/main/java/io/humanbest/hex/dec/Dec.java
@@ -3,8 +3,16 @@ package io.humanbest.hex.dec;
 import java.util.ArrayList;
 
 public class Dec {
-    
-    public ArrayList<Card> cardList = new ArrayList<>();
 
-    public Dec() {}
+    public ArrayList<Card> remainCardList = new ArrayList<>();
+    public ArrayList<Card> selectedCardList = new ArrayList<>();
+    public ArrayList<Card> usedCardList = new ArrayList<>();
+
+    public Dec(ArrayList<Card> remainCardList, ArrayList<Card> selectedCardList, ArrayList<Card> usedCardList) {
+        this.remainCardList = remainCardList;
+        this.selectedCardList = selectedCardList;
+        this.usedCardList = usedCardList;
+    }
+
+
 }
