794308ae2571f92eca24869e6d242e793b3beed1
95suskjg <95suskjg@naver.com>
2022-06-22 15:23:31 +0900

    20220622

--- src/main/java/io/humanbest/hex/dec/Card.java
+++ src/main/java/io/humanbest/hex/dec/Card.java
@@ -3,16 +3,18 @@ package io.humanbest.hex.dec;
 public class Card {
 
     public static enum Type {
-        ATTACK, DEFNSE, BUFF, DEBUFF
+        ATTACK, DEFNSE, ATBUFF, DFBUFF, DEBUFF
     }
 
     public Type type;
+    public int typeValue;
     public int cost;
     public String name;
     public String description;
 
-    public Card(Type type, int cost, String name) {
+    public Card(Type type,int typeValue, int cost, String name) {
         this.type = type;
+        this.typeValue = typeValue;
         this.cost = cost;
         this.name = name;
     }
