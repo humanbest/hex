11a395949d2c95ae809c3a6ed616254630be32a3
95suskjg <95suskjg@naver.com>
2022-06-24 08:23:55 +0900

    20220624

---
+++ src/main/java/io/humanbest/hex/Type.java
@@ -0,0 +1,12 @@
+package io.humanbest.hex;
+
+public class Type {
+
+    public String name;
+    public int value;
+
+    public Type(String name, int value){
+        this.name = name;
+        this.value = value;
+    }
+}
--- src/main/java/io/humanbest/hex/card/Card.java
+++ src/main/java/io/humanbest/hex/card/Card.java
@@ -1,23 +1,34 @@
 package io.humanbest.hex.card;
 
+import io.humanbest.hex.Type;
+
+import java.util.ArrayList;
+
 public class Card {
 
     //AT버프는 몬스터에게 피해를 입히는 공격적인 효과를 지닌 버프입니다
     //DF버프는 수비적인 효과를 얻을 수 있는 버프입니다
+//    public static enum Type {
+//        ATTACK, DEFNSE, ATBUFF, DFBUFF, DEBUFF
+//    }
 
-    public static enum Type {
-        ATTACK, DEFNSE, ATBUFF, DFBUFF, DEBUFF
-    }
-
-    public Type type;
+    public ArrayList<Type> cardTypeList;
     public String name;
     public int typeValue;
     public int cost;
-    
-    public Card(String name, Type type, int typeValue, int cost) {
+
+    public Card(String name, Type cardType, int typeValue, int cost) {
         this.name = name;
-        this.type = type;
+        this.cardTypeList = new ArrayList<>();
+        this.cardTypeList.add(cardType);
         this.typeValue = typeValue;
         this.cost = cost;
     }
+    public Card(String name, ArrayList<Type> cardTypeList, int typeValue, int cost) {
+        this.name = name;
+        this.cardTypeList = cardTypeList;
+        this.typeValue = typeValue;
+        this.cost = cost;
+    }
+
 }
