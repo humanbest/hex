a925d37962a70cb94afc35f950589fb7336011bd
whdlsrnr0731 <whdlsrnr0731@gmail.com>
2022-08-02 11:40:09 +0900

    init

---
+++ src/test/java/kr/kro/hex/HexApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.hex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
