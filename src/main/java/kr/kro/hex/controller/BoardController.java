57ae2cdee9e26cfa13d0d42f900a8ce734852a29
ChoInguk <whdlsrnr0731@gmail.com>
2022-08-22 19:21:55 +0900

    update

--- src/main/java/kr/kro/hex/controller/BoardController.java
+++ src/main/java/kr/kro/hex/controller/BoardController.java
@@ -90,7 +90,7 @@ public class BoardController {
     public String updateBoardView(Board board, Model model) {
         model.addAttribute("board", boardService.getBoard(board));
         model.addAttribute("categoryList", categoryService.getCategoryList());
-        return "insertBoard";
+        return "/board/insertBoard";
     }
 
     /**
