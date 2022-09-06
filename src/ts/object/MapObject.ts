14cdce0d8f7e2135eada6cc1d02f276c785e2e38
yhy5847 <yhy5847@gmail.com>
2022-09-06 02:09:30 +0000

    Fixed #79: 맵씬 맵오브젝트 분리, 맵 매니져 삭제

--- src/ts/object/MapObject.ts
+++ src/ts/object/MapObject.ts
@@ -1,6 +1,6 @@
 import MapScene from "../scene/MapScene";
 
-export default class Nodes extends Phaser.GameObjects.Container {
+export default class MapObject extends Phaser.GameObjects.Container {
 
     /** 노드 깊이 설정 */
     private static readonly DEPTH = 7;
@@ -11,19 +11,18 @@ export default class Nodes extends Phaser.GameObjects.Container {
     /** 노드 배열 설정 */
     private static setNodeData(scene: Phaser.Scene): void 
     {
-        const start: number = 1300;
+        const start: number = 1400;
         const step: number = 100;
 
-        Nodes.NODE_ARR = [];
-        Nodes.NODE_ARR.push({type: NodeType.START, depth: 0, x: scene.game.canvas.width/4 + 300, y: start});
-        for(let i = 1; i < Nodes.DEPTH; i++) {
+        MapObject.NODE_ARR = [];
+        MapObject.NODE_ARR.push({type: NodeType.START, depth: 0, x: scene.game.canvas.width/4 + 300, y: start});
+        for(let i = 1; i < MapObject.DEPTH; i++) {
             const n = (Math.random() * 3 + 3)|0;
-            console.log(n);
             for(let j = 0; j < n; j++) {
-                Nodes.NODE_ARR.push({type: Math.random() < 0.8 ? NodeType.BATTLE : NodeType.SHOP, depth: i, x: scene.game.canvas.width/4 + (j * 100) + 600/n, y: start - (i * step)});
+                MapObject.NODE_ARR.push({type: Math.random() < 0.8 ? NodeType.BATTLE : NodeType.SHOP, depth: i, x: scene.game.canvas.width/4 + (j * 100) + 600/n, y: start - (i * step)});
             }
         }
-        Nodes.NODE_ARR.push({type: NodeType.BOSS, depth: Nodes.DEPTH, x: scene.game.canvas.width/4 + 300, y: start - (Nodes.DEPTH * 100)});
+        MapObject.NODE_ARR.push({type: NodeType.BOSS, depth: MapObject.DEPTH, x: scene.game.canvas.width/4 + 300, y: start - (MapObject.DEPTH * step)});
     }
 
     constructor(scene: MapScene, x: number = 0, y: number = 0) {
@@ -31,13 +30,13 @@ export default class Nodes extends Phaser.GameObjects.Container {
         super(scene, x, y);
 
         // 노드 정보가 없으면 노드 배열 생성
-        if(Nodes.NODE_ARR === undefined) Nodes.setNodeData(scene);
+        if(MapObject.NODE_ARR === undefined) MapObject.setNodeData(scene);
         
         // 지도 이미지 추가
         this.add(scene.add.image(-100, scene.game.canvas.height/2, MapScene.KEY.IMAGE.MAIN_MAP).setScale(0.6).setOrigin(0).setDepth(1))
 
         // 노드 이미지 추가
-        Nodes.NODE_ARR.forEach(node => this.add(scene.add.image(node.x, node.y, node.type)).setDepth(3));
+        MapObject.NODE_ARR.forEach(node => this.add(scene.add.image(node.x, node.y, node.type)).setDepth(3));
 
         // 씬에 맵 컨테이너 추가
         scene.add.existing(this);
