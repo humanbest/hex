020a8876ea2f15fe2d9e67ecbc390e27ef94b13e
95suskjg <95suskjg@naver.com>
2022-09-14 10:52:27 +0900

    2022 09 14 / 여관씬 휴식 버튼 추가

--- src/ts/scene/InnScene.ts
+++ src/ts/scene/InnScene.ts
@@ -6,7 +6,8 @@ export default class InnScene extends Scene{
     static readonly KEY = {
         NAME: "InnScene",
         IMAGE: {
-            Inn:"Inn"
+            Inn:"Inn",
+            rest:"rest"
         }
     }
 
@@ -26,16 +27,31 @@ export default class InnScene extends Scene{
         })
 
         this.load.image(InnScene.KEY.IMAGE.Inn, "assets/images/innScene/Inn.png");
+        this.load.image(InnScene.KEY.IMAGE.rest, "assets/images/innScene/rest.png");
 
     }
 
     create(): void {
 
+        const background = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, InnScene.KEY.IMAGE.Inn);
+        background.setScale(this.cameras.main.width / background.width, this.cameras.main.height / background.height).setScrollFactor(0);
+
         new TopMenu(this, 0, 0);
 
-        console.log("dd");
+        const restbutton = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2-30,InnScene.KEY.IMAGE.rest);
+        restbutton.setScale(0.45)
+            .setInteractive()
+            .on("pointerover", () => this.add.tween({
+                targets: restbutton,
+                duration: 70,
+                scale: 0.47
+            }))
+            .on("pointerout", () => this.add.tween({
+                targets: restbutton,
+                duration: 70,
+                scale: 0.45
+            }));
 
-        this.add.image(300,300, InnScene.KEY.IMAGE.Inn);
 
     }
 
