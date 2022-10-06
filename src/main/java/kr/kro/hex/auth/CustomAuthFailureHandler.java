7cb1c7d04cb75cf720121ef6e935579a9ba69416
Jeong <cane04765@gmail.com>
2022-10-06 11:43:19 +0900

    admin 컨트롤러 수정

--- src/main/java/kr/kro/hex/auth/CustomAuthFailureHandler.java
+++ src/main/java/kr/kro/hex/auth/CustomAuthFailureHandler.java
@@ -19,6 +19,7 @@ public class CustomAuthFailureHandler extends SimpleUrlAuthenticationFailureHand
 			AuthenticationException exception) throws IOException, ServletException {
 
                 String errorMessage;
+                
                 if(exception instanceof BadCredentialsException){
                     errorMessage = "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해 주세요.";
                 } else if(exception instanceof InternalAuthenticationServiceException){
