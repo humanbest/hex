485489694bfd37a9901e75f501d57ff44eaba5e6
Rubisco <whdlsrnr0731@gmail.com>
2022-10-07 21:38:24 +0900

    Fixed: 어드민 컨트롤러 사용되지 않는 의존성 제거

--- src/main/java/kr/kro/hex/controller/AdminController.java
+++ src/main/java/kr/kro/hex/controller/AdminController.java
@@ -1,20 +1,13 @@
 package kr.kro.hex.controller;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.kro.hex.domain.Board;
 import kr.kro.hex.domain.Member;
