0bb4fd075250616e2d75fdeb1317c7d1b2920e9e
Rubisco <whdlsrnr0731@gmail.com>
2022-10-09 01:03:44 +0900

    Modify: 로그인 에러메시지를 post로 전송하여 url에 표시되지 않도록 수정

--- src/main/java/kr/kro/hex/controller/SecurityController.java
+++ src/main/java/kr/kro/hex/controller/SecurityController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.RequiredArgsConstructor;
 
@@ -31,11 +32,16 @@ public class SecurityController {
     }
     
     @GetMapping(params={"act=login"})
-    public String getLoginView(@RequestParam(value = "error", required = false)String error,
-        @RequestParam(value = "exception", required = false)String exception,Model model) {
-        
-        model.addAttribute("error", error);
-        model.addAttribute("exception", exception);
+    public String getLoginView(
+        @RequestParam(value = "error", required = false)String error, 
+        @RequestParam(value = "exception", required = false)String exception, 
+        RedirectAttributes redirectAttribute, 
+        Model model
+    ) {
+        if(exception != null) {
+            redirectAttribute.addFlashAttribute("exception", exception);
+            return "redirect:/auth?act=login";
+        }
         return "member/login";
     }
     
